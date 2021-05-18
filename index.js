bbfa5fbffded543d2da46793ba64cbdda142a9d2
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-18 20:57:15 +0000

    Done.

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`;
   }
 }
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
   }
 });
