5b65b9a7d0dc4e998f55e8315c022620af4f0f20
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-18 20:54:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
